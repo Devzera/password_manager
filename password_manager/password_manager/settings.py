1cba9aaf41634578160ba5c91efb23b505bebfd4
Vadim <vadimatenov@icloud.com>
2022-10-18 16:11:25 +0300

    add new application(storage)

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'generator.apps.GeneratorConfig'
+    'generator.apps.GeneratorConfig',
+    'storage.apps.StorageConfig',
 ]
 
 MIDDLEWARE = [
